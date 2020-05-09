0fffe851c73e263f1fabe051695f973a0dd5a14e
chinwahc <cchinwah1@gmail.com>
2020-05-09 21:18:14 +0100

    First commit

--- config/db.js
+++ config/db.js
@@ -1,8 +1,7 @@
 var db = require('mongoose');
 const config = require('config');
 
-db.connect(config.get('mongodb+srv://chinwahc:Chelsea4life@footy-db-ss1aq.mongodb.net/test?retryWrites=true&w=majority'), { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
-//db.connect(config.get('database.url'), { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+db.connect(config.get('database.url'), { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 
 
 module.exports = db;
