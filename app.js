d31e2890e77488bcfa422bb89404c233d0e8cd3d
chinwahc <cchinwah1@gmail.com>
2020-05-24 14:55:06 +0100

    updated config

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ let express = require('express');
 let app = express();
 let path = require('path');
 let bodyParser = require('body-parser');
-const port = process.env.PORT || 3001;
 const { PORT, CLIENT_ORIGIN, DB_URL } = require('./config/config')
 
 //configure express app to parse json content and form data
-//app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 let mongoose = require('mongoose');
 const config = require('config');
@@ -16,7 +14,6 @@ app.use('/api/', require('./routes/players'));
 //configure express app to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 //connect to mongodb instance where database is footydb
-//mongoose.connect(config.get('database.url'), { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 mongoose.connect(DB_URL, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false });
 
 var db = mongoose.connection;
@@ -27,7 +24,7 @@ db.once('open', function callback() {
 
 // default route
 app.get('/', (req, res) => {
-    res.json("Welcome to Footy API. To get all players, add '/players' to the url.");
+    res.json("Welcome to the Footballr API. This API is designed to help serve data to the Football App. To get all players, add '/api/players' to the url.");
 });
 //listen on port 3001
 app.listen(PORT, () => {
